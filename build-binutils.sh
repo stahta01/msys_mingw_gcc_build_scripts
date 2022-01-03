0559a73c35ccbf3da2c341c705c7788f99efd016
Tim Stahlhut <stahta01@gmail.com>
2022-01-03 06:15:36 -0500

    script: Add build-binutils.sh

---
+++ build-binutils.sh
@@ -0,0 +1,7 @@
+INSTALL_PATH="`pwd`/local"
+
+mkdir -p binutils-build && cd binutils-build && \
+../binutils-2.32/configure --prefix="$INSTALL_PATH" --with-libiconv-prefix="$INSTALL_PATH" --enable-nls --disable-werror && \
+make && \
+make install && \
+make check -k
