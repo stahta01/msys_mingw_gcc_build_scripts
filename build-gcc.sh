4f7b5392872d4b669c6c46c6cfbc8d0562ab083c
Tim Stahlhut <stahta01@gmail.com>
2022-01-04 00:19:04 -0500

    script: Edit build-gcc.sh
    Add build log and delete build folder

--- build-gcc.sh
+++ build-gcc.sh
@@ -1,14 +1,17 @@
 INSTALL_PATH="`pwd`/local"
 
+[[ -d gcc-build ]] && rm -rf gcc-build && \
 mkdir -p gcc-build && cd gcc-build && \
+date --rfc-3339=seconds > ../gcc-build.log && \
 ../gcc-9.2.0/configure --prefix="$INSTALL_PATH" \
   --with-libiconv-prefix="$INSTALL_PATH" --with-libintl-prefix="$INSTALL_PATH" \
   --with-gmp="$INSTALL_PATH" --with-mpfr="$INSTALL_PATH" --with-mpc="$INSTALL_PATH" \
   --enable-static --enable-shared \
   --disable-libvtv --disable-win32-registry \
   --with-dwarf2 --disable-sjlj-exceptions \
-  --enable-languages=c,c++,ada \
-  --enable-nls --disable-werror --disable-build-format-warnings && \
-make && \
+  --enable-languages=c,c++ \
+  --enable-nls --disable-werror --disable-build-format-warnings 2>&1 | tee ../gcc-build.log && \
+make 2>&1 | tee ../gcc-build.log && \
+date --rfc-3339=seconds >> ../gcc-build.log && \
 make install && \
 make check -k
