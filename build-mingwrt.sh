b099e75d5c4d32e9e2ae8d602b305066f691c505
Tim Stahlhut <stahta01@gmail.com>
2022-01-06 09:41:33 -0500

    script: Edit build-mingwrt.sh
    INSTALL_PATH='/usr/local'

--- build-mingwrt.sh
+++ build-mingwrt.sh
@@ -1,4 +1,8 @@
-INSTALL_PATH="`pwd`/local"
+if [[ $# -ne 0 ]]; then
+  INSTALL_PATH="$1"
+else
+  INSTALL_PATH='/usr/local'
+fi
 
 mkdir -p "$INSTALL_PATH" && \
 mkdir -p  mingwrt-build && cd  mingwrt-build && \
