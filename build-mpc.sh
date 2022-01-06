4848b942dc9182527ef01b7466af43b16972d082
Tim Stahlhut <stahta01@gmail.com>
2022-01-06 09:41:54 -0500

    script: Edit build-mpc.sh
    INSTALL_PATH='/usr/local'

--- build-mpc.sh
+++ build-mpc.sh
@@ -1,4 +1,8 @@
-INSTALL_PATH="`pwd`/local"
+if [[ $# -ne 0 ]]; then
+  INSTALL_PATH="$1"
+else
+  INSTALL_PATH='/usr/local'
+fi
 
 mkdir -p mpc-build && cd mpc-build && \
 ../mpc-1.0.3/configure --prefix="$INSTALL_PATH" --with-gmp="$INSTALL_PATH" --with-mpfr="$INSTALL_PATH" --enable-static --disable-shared && \
