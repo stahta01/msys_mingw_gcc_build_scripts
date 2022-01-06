93426fbac36ae9caba9a035c5cd3a6338bc4b31e
Tim Stahlhut <stahta01@gmail.com>
2022-01-06 09:42:10 -0500

    script: Edit build-mpfr.sh
    INSTALL_PATH='/usr/local'

--- build-mpfr.sh
+++ build-mpfr.sh
@@ -1,4 +1,8 @@
-INSTALL_PATH="`pwd`/local"
+if [[ $# -ne 0 ]]; then
+  INSTALL_PATH="$1"
+else
+  INSTALL_PATH='/usr/local'
+fi
 
 mkdir -p mpfr-build && cd mpfr-build && \
 ../mpfr-3.1.4/configure --prefix="$INSTALL_PATH" --with-gmp="$INSTALL_PATH" --enable-thread-safe --enable-static --disable-shared && \
