c4b55beac80bed8ace64dc2a04588f25999ec502
Tim Stahlhut <stahta01@gmail.com>
2022-01-06 22:26:01 -0500

    script: Use 5.4.2 in build-w32api.sh

--- build-w32api.sh
+++ build-w32api.sh
@@ -6,7 +6,7 @@ fi
 
 mkdir -p "$INSTALL_PATH" && \
 mkdir -p  w32api-build && cd  w32api-build && \
-../w32api-5.3.4/configure --prefix="$INSTALL_PATH" && \
+../w32api-5.4.2/configure --prefix="$INSTALL_PATH" && \
 make && \
 make install && \
 make check -k
