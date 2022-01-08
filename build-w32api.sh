859da707324a42cd058c863943457d533de4a1b7
Tim Stahlhut <stahta01@gmail.com>
2022-01-07 22:34:52 -0500

    script: Add target=i686-pc-mingw32 to build-w32api.sh

--- build-w32api.sh
+++ build-w32api.sh
@@ -5,8 +5,8 @@ else
 fi
 
 mkdir -p "$INSTALL_PATH" && \
-mkdir -p  w32api-build && cd  w32api-build && \
-../w32api-5.4.2/configure --prefix="$INSTALL_PATH" && \
+mkdir -p w32api-build && cd w32api-build && \
+../w32api-5.4.2/configure --target=i686-pc-mingw32 --prefix="$INSTALL_PATH" && \
 make && \
 make install && \
 make check -k
