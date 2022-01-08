2a05c63b946345b3bb6b78171a971501fbfb0e4f
Tim Stahlhut <stahta01@gmail.com>
2022-01-08 00:00:15 -0500

    script: Remove script calls in build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -2,10 +2,5 @@ INSTALL_PATH='/usr/local'
 
 ./build-w32api.sh "$INSTALL_PATH" && \
 ./build-mingwrt.sh "$INSTALL_PATH" && \
-./build-zlib.sh "$INSTALL_PATH" && \
-./build-libiconv.sh "$INSTALL_PATH" && \
 ./build-binutils.sh "$INSTALL_PATH" && \
-./build-gmp.sh "$INSTALL_PATH" && \
-./build-mpfr.sh "$INSTALL_PATH" && \
-./build-mpc.sh "$INSTALL_PATH" && \
 ./build-gcc.sh "$INSTALL_PATH"
