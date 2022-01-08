2cec26681ed163ea379fdb9cd64c5ecc8d9d4728
Tim Stahlhut <stahta01@gmail.com>
2022-01-08 07:13:37 -0500

    script: Remove build folder in build-mingwrt.sh

--- build-mingwrt.sh
+++ build-mingwrt.sh
@@ -4,6 +4,8 @@ else
   INSTALL_PATH='/usr/local'
 fi
 
+[[ -d mingwrt-build ]] && rm -rf mingwrt-build
+
 mkdir -p "$INSTALL_PATH" && \
 mkdir -p  mingwrt-build && cd  mingwrt-build && \
 ../mingwrt-5.4.2/configure --build=i686-pc-mingw32 --prefix="$INSTALL_PATH" && \
